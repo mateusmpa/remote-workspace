804e2a424aefca273e14d1eed7922aa0926e2590
Mateus Alves <mateusmpa@gmail.com>
2016-07-05 21:49:06 -0300

    Model workspace

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160702012139) do
+ActiveRecord::Schema.define(version: 20160706004733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20160702012139) do
     t.string   "title"
     t.string   "description"
     t.string   "text"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "workspaces", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
---
+++ spec/factories/workspaces.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :workspace do
+    title "MyString"
+    description "MyText"
+  end
+end
